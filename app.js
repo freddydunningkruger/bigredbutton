0dd93fa0b25528b6eee02e513c8a8bebac937bc9
supersecretevilmegallc.eth <46723803+freddydunningkruger@users.noreply.github.com>
2023-04-13 14:57:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,80 @@ const contractAddress = '0xc4e0e75bEcFB3193916Dddc0Dee3970653F3f79B'; // Add the
 let web3;
 let contractInstance;
 let userAccount;
-let contractABI;
 
-async function loadContractABI() {
-    const response = await fetch('./EthTimerABI.json');
-    const abiJson = await response.json();
-    contractABI = abiJson;
-}
+const contractABI = [
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "newEndTime",
+        "type": "uint256"
+      }
+    ],
+    "name": "TimerExtended",
+    "type": "event"
+  },
+  {
+    "inputs": [],
+    "name": "endTime",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "lastUser",
+    "outputs": [
+      {
+        "internalType": "address payable",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "extendTimer",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function",
+    "payable": true
+  },
+  {
+    "inputs": [],
+    "name": "getRemainingTime",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  }
+];
 
 document.addEventListener('DOMContentLoaded', async () => {
-    await loadContractABI();
-
     const connectWallet = document.getElementById('connectWallet');
     const pushButton = document.getElementById('pushButton');
     const counter = document.getElementById('counter');
