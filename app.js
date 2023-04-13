24d8f79fab27fe07d71107810ab1229c32df4cf0
supersecretevilmegallc.eth <46723803+freddydunningkruger@users.noreply.github.com>
2023-04-13 13:11:02 -0400

    wallet

--- app.js
+++ app.js
@@ -2,20 +2,43 @@ const Web3 = require('web3');
 const contractABI = []; // Add the ABI from the compiled contract
 const contractAddress = ''; // Add the deployed contract address
 
-const web3 = new Web3(Web3.givenProvider);
-const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+let web3;
+let contractInstance;
+let userAccount;
 
 document.addEventListener('DOMContentLoaded', async () => {
-    const accounts = await web3.eth.getAccounts();
+    const connectWallet = document.getElementById('connectWallet');
     const pushButton = document.getElementById('pushButton');
     const counter = document.getElementById('counter');
     const timer = document.getElementById('timer');
 
+    connectWallet.addEventListener('click', async () => {
+        if (window.ethereum) {
+            web3 = new Web3(window.ethereum);
+            try {
+                await window.ethereum.request({ method: 'eth_requestAccounts' });
+                const accounts = await web3.eth.getAccounts();
+                userAccount = accounts[0];
+                contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+            } catch (err) {
+                console.error('User denied account access', err);
+            }
+        } else {
+            console.error('Non-Ethereum browser detected. You should consider trying MetaMask!');
+        }
+    });
+
     pushButton.addEventListener('click', async () => {
-        await contractInstance.methods.extendTimer().send({ from: accounts[0], value: web3.utils.toWei('0.01', 'ether') });
+        if (!contractInstance) {
+            alert('Please connect your wallet first');
+            return;
+        }
+        await contractInstance.methods.extendTimer().send({ from: userAccount, value: web3.utils.toWei('0.01', 'ether') });
     });
 
     setInterval(async () => {
+        if (!contractInstance) return;
+
         const contractBalance = await web3.eth.getBalance(contractAddress);
         counter.innerText = `Contract Balance: ${web3.utils.fromWei(contractBalance, 'ether')} ETH`;
 
